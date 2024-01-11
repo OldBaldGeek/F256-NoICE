4676b3eb0bc27c2cf8a2b71c3e7f0ec059349a37
OldBaldGeek <>
2024-01-10 20:10:35 -0600

    Updated to run Mon6502_F256_kernel.pgz as the monitor
    Uses stop/start to load user files, thus avoiding reset of the kernel.

--- noi_runner.bat
+++ noi_runner.bat
@@ -11,36 +11,33 @@
 :: 5) Select "OK"
 ::
 :: When NoICE runs on the PC, it will invoke this file to load the monitor.
+::
 :: When you tell NoICE to LOAD a file, the name of the file will be passed
-:: to this file. The file will be loaded, and the monitor reloaded since fnxmgr
-:: may overwrite the reset vector, and it resets the target.
+:: to this file. We stop the target (which is in the NoICE monitor), load
+:: the file, and resume target execution
 
 :: The loader we are using (your path will probably differ)
-:: TODO: normal set fnx="\utilities\fnxmgr.zip"
-set fnx="C:\repos\FoenixMgr\FoenixMgr\fnxmgr.py"
+:: set fnx="C:\repos\FoenixMgr\FoenixMgr\fnxmgr.py"
+set fnx="\utilities\fnxmgr.zip"
 
 :: The monitor to load (your path will probably differ)
-set mon="C:\repos\F256-NoICE\Mon6502_F256_Merlin.pgz"
-
-:: Put the target CPU into reset until we do ALL our commands
-:: This also suspends reset/release by further invocations until we release_reset
-python %fnx% --assert_reset
-if not %ERRORLEVEL% == 0 pause
-
-:: This version of the monitor owns the interrupt vectors and lives at F900
-python %fnx% --boot RAM
-if not %ERRORLEVEL% == 0 pause
+set mon="C:\repos\F256-NoICE\Mon6502_F256_kernel.pgz"
 
 :: If we have a filename parameter, load it (program to debug)
 IF "%~1" == "" goto nofile
+
+python %fnx% --stop
+if not %ERRORLEVEL% == 0 pause
+
 python %fnx% --upload-srec %1
 if not %ERRORLEVEL% == 0 pause
-:nofile
 
-:: Load the NoICE monitor, which includes reset vector
-python %fnx% --run-pgz %mon%
+python %fnx% --start
 if not %ERRORLEVEL% == 0 pause
+exit
 
-:: Release the target CPU from RESET, causing the monitor to run.
-python %fnx% --release_reset
+
+:nofile
+:: Load the NoICE monitor
+python %fnx% --run-pgz %mon%
 if not %ERRORLEVEL% == 0 pause
